93ecd96b31cc0584b569408f8c6e4d5968115203
laikaixiang <118657294+laikaixiang@users.noreply.github.com>
2026-05-10 22:34:16 +0800

    Revert "新增功能：markdown显示"
    This reverts commit dbdfcd67

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import 'katex/dist/katex.min.css'
 import './styles/main.css'
 
 const app = createApp(App)
